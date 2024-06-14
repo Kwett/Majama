c860fe0754891d2224e7ead8744cbae75707deb6
Kwett <coudrin.clement@gmail.com>
2024-06-14 21:03:12 +0200

    implements home page

--- majama/src/app/home/home.component.ts
+++ majama/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Card } from './models/card.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  menuCards: Card[] = [
+    {title: 'Jam Session', image: 'jam', route: '/jam'},
+    {title: 'Concerts', image: 'concert', route: '/concert'},
+    {title: 'News', image: 'news', route: '/news'},
+    {title: 'Nos Artistes', image: 'roaster', route: '/roaster'},
+    {title: 'Prestations', image: 'presta', route: '/prestation'},
+    {title: 'Contact', image: 'contact', route: '/contact'}
+  ];
+
+  constructor(protected router: Router) {}
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
